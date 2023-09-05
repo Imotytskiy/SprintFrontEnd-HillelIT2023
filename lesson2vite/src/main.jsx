23c8fcefd11a7fa8d49e82eed44dbd2505b48e62
Imotytskiy <Imotytskiy@gmail.com>
2023-09-06 00:53:00 +0200

    add

--- lesson2vite/src/main.jsx
+++ lesson2vite/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Footer from "./components/footer/footer";
 import Body from "./components/body/body";
+import SectionLearn from "./components/sectionlearn/section_learn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Body />
+    <SectionLearn />
     <Footer />
   </React.StrictMode>
 );
