be7367567b3ea076bb7dcec69b57aee129a47a5b
Ihor MOtytskiy <Imotytskiy@gmail.com>
2023-09-06 11:47:30 +0200

    add

--- lesson2vite/src/components/Navbar/navbar.jsx
+++ lesson2vite/src/components/Navbar/navbar.jsx
@@ -1,39 +1,39 @@
-import React from "react";
+
 import logo from "../../assets/logohead.png";
 import "./navbar.css";
 const Navbar = () => {
   return (
     <header className="navbar">
-      <a class="navbar_a" href="index.html">
+      <a className="navbar_a" href="index.html">
         <img src={logo} alt="logo" />
         UpDate
       </a>
       <nav>
         <ul>
           <li>
-            <a class="navbar_a" href="#">
+            <a className="navbar_a" href="#">
               Courses
             </a>
           </li>
           <li>
-            <a class="navbar_a" href="#">
+            <a className="navbar_a" href="#">
               Teachers
             </a>
           </li>
           <li>
-            <a class="navbar_a" href="#">
+            <a className="navbar_a" href="#">
               Blog
             </a>
           </li>
         </ul>
       </nav>
-      <a class="navbar_a" href="#">
+      <a className="navbar_a" href="#">
         EN
       </a>
-      <a class="navbar_a" href="#">
+      <a className="navbar_a" href="#">
         Sign IN
       </a>
-      <a class="navbar_a" href="#">
+      <a className="navbar_a" href="#">
         Trial Free
       </a>
     </header>
