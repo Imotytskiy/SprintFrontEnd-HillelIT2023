8016474b70a66909f5dae29a2cee6e4903ba480f
Imotytskiy <Imotytskiy@gmail.com>
2023-09-13 16:07:25 +0200

    add

--- lesson2vite/src/components/body/body.jsx
+++ lesson2vite/src/components/body/body.jsx
@@ -1,15 +1,15 @@
 import Navbar from "../navbar/navbar";
-// import SectionLearn from "../sectionlearn/section_learn";
+import SectionLearn from "../sectionlearn/section_learn";
 import Footer from "../footer/footer";
-// import men from "./img/men.png";
-// import rectangle from "./img/rectangle.svg";
+import men from "./img/men.png";
+import rectangle from "./img/rectangle.svg";
 import "./body.css";
 
 const Body = () => {
   return (
     <main className="body container">
       <Navbar />
-      {/* <section className="offer container">
+      <section className="offer container">
         <div className="offer__holder">
           <img src="" alt="" />
           <h1 className="offer__title">
@@ -18,26 +18,23 @@ const Body = () => {
           <p className="offer__description">
             Start learning <a href="">free</a>
           </p>
-          <button className="">Chooose a course</button>
-          <p>50%</p>
+          <button className="offer__button">Chooose a course</button>
+          <p className="offer__sale">50%</p>
           <p className="offer__time">
             <time>24 days 17:44:16</time>
           </p>
         </div>
 
-        <ul>
-          <li>Marketer</li>
-          <li>Illustrator</li>
-          <li>Programmer</li>
-          <li>Designer</li>
-          <li>Photographer</li>
-          <li>Marketer</li>
+        <ul className="off  er__list">
+          <li className="offer_marketer offer__item">Marketer</li>
+          <li className="offer_animator offer__item">Animator</li>
+          <li className="offer_illustrator offer__item">Illustrator</li>
+          <li className="offer_programmer offer__item">Programmer</li>
+          <li className="offer_designer offer__item">Designer</li>
+          <li className="offer_photographer offer__item">Photographer</li>
         </ul>
-
-        <img className="offer__men" src={men} alt="men" />
-        <img className="offer__rectangle" src={rectangle} alt="rectangle" />
-      </section> */}
-      {/* <SectionLearn /> */}
+      </section>
+      <SectionLearn />
       <Footer />
     </main>
   );
